e7383ed1334ae054fb1291a0040720048265a39a
= <koalateac@gmail.com>
2017-02-07 10:10:55 -0500

    config changes and various app stuff

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -16,3 +17,11 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, drinks=drinks)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if(form.validate_on_submit()):
+        flash('Login for username="%s" and password="%s"' % (form.username.data, form.password.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
