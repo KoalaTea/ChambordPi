bb34c102cd518e333320e3f4cfaf12978957d237
= <koalateac@gmail.com>
2017-02-06 21:58:58 -0500

    some basic set up

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "First set of code"
+    user = { "nickname": "koalatea" }
+    drinks = [
+        {
+            'drink_name' : 'Godfather',
+            'ingredients' : [ 'half ameretto', 'half scotch whiskey' ]
+        },
+        {
+            'drink_name' : 'temp test',
+            'ingredients' : [ 'nothing', 'more nothing' ]
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, drinks=drinks)
