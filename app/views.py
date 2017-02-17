9aec32ffd656d7829e34a537289926f5161da3e1
kcarretto <kcarretto@gmail.com>
2017-02-17 01:04:18 -0500

    Added minor database tweak and styling to recent_orders

--- app/views.py
+++ app/views.py
@@ -312,7 +312,7 @@ def order_drink():
                  "name": drink['name'],
                  "cost": drink['cost'],
                  "type": drink['type'],
-                 "recipe": drink['recipe'][0],
+                 "recipe": drink['recipe'],
                  "image": drink['image'],
                  "timeOrdered": time.time(),
                  "user": current_user.username,
